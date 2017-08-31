66fbecafeba0c5dc055bcb0f9fd06ad3c916f2d8
User <User@10.30.16.44>
2017-08-30 21:44:04 -0500

    added getters and setters to first lab

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -4,14 +4,15 @@ import java.util.Date;
 
 /**
  * Fix the code in this class to do PROPERTY encapsulation correctly. Also
- * consider if any of the properties should be mandatory and use a 
- * constructor to enforce that. Review the tips in the document 
- * "EncapCheckList.pdf" if needed.
+ * consider if any of the properties should be mandatory and use a constructor
+ * to enforce that. Review the tips in the document "EncapCheckList.pdf" if
+ * needed.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.02
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.02
  */
 public class Employee {
+
     private String firstName;
     private String lastName;
     private String ssn;
@@ -21,15 +22,85 @@ public class Employee {
     private boolean movedIn;
     private String cubeId;
     private Date orientationDate;
-    
-    
+
     public Employee(String firstName, String lastName, String ssn) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.ssn = ssn;
     }
-   
 
+    /* Setters */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public void setMetWithHr(boolean metWithHr) {
+        this.metWithHr = metWithHr;
+    }
+
+    public void setMetDeptStaff(boolean metDeptStaff) {
+        this.metDeptStaff = metDeptStaff;
+    }
+
+    public void setReviewedDeptPolicies(boolean reviewedDeptPolicies) {
+        this.reviewedDeptPolicies = reviewedDeptPolicies;
+    }
+
+    public void setMovedIn(boolean movedIn) {
+        this.movedIn = movedIn;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
+
+    /* Getters */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public boolean isMetWithHr() {
+        return metWithHr;
+    }
+
+    public boolean isMetDeptStaff() {
+        return metDeptStaff;
+    }
+
+    public boolean isReviewedDeptPolicies() {
+        return reviewedDeptPolicies;
+    }
+
+    public boolean isMovedIn() {
+        return movedIn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
 
 }
