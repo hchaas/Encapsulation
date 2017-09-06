1ae6b3699b47252dc55543eb8a8f3c8f7057b52f
User <User@10.30.16.44>
2017-09-06 18:14:07 -0500

    /*I had trouble understanding this lab I think in part because I was thinking about it too literally in how each person would interface with the software. To better understand it, I studied the solution and copied it over, changing the variable and method names so that I could walk through each part of the code, understanding how it fit together.

--- src/lab4/Startup.java
+++ src/lab4/Startup.java
@@ -9,13 +9,17 @@ package lab4;
  * @author      Jim Lombardo, WCTC Instructor
  * @version     1.02
  */
+
+/*I had trouble understanding this lab I think in part because I was thinking about it too literally in how
+each person would interface with the software. To better understand it, I studied the solution and copied
+it over, changing the variable and method names so that I could walk through each part of the code, understanding
+how it fit together.
+*/
 public class Startup {
     public static void main(String[] args) {
+        Organization organization = new Organization();
+        organization.hireNewEmployee("Peter", "Piper", "333-33-3333");
 
-        Employee employee = new Employee("Peter","Piper","333-33-3333");
-        
-        employee.doFirstTimeOrientation("A101");
-        employee.getReportService().outputReport();
     }
     
     
